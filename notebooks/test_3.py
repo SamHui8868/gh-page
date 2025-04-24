cf65b8e63b82ca27a350b18998bfeaae3ae3bf17
sam <sam>
2025-04-24 22:07:23 +0900

    final1

---
+++ notebooks/test_3.py
@@ -0,0 +1,20 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.17.0
+# ---
+
+# %% [markdown]
+# # Test Notebook
+# This is a test page to verify GitHub Pages deployment
+
+# %%
+print("You did it!!!")
+
+
+# %%
+print("Hello, this is a test notebook!!!")
