9215849058e52e12f7ddda6799d251aa05244ee7
sam <sam>
2025-04-24 20:48:19 +0900

    upload

---
+++ notebooks/test.py
@@ -0,0 +1,12 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.17.0
+# ---
+
+# %%
+print("Hello")
